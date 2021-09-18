355391a047a9e14c3f3d82a2973e8c1cfe937cb4
Yothin Muangsommuk <m@yothinix.com>
2021-09-18 14:44:14 +0700

    Adding logic to fetch event and assign to props

--- frontend/src/routes/index.svelte
+++ frontend/src/routes/index.svelte
@@ -1,4 +1,28 @@
+<script lang="ts" context="module">
+    import type { Load } from '@sveltejs/kit';
+
+    export const load: Load = async ({ fetch }) => {
+      const res = await fetch('/events');
+      const data = await res.json();
+
+      return { props: { events: data } };
+    };
+</script>
+
 <script lang="ts">
+  type Habit = {
+    id: number,
+    name: string,
+    category: string
+  }
+
+  type Event = {
+    id: number;
+    habit: Habit;
+    created_at: string;
+  }
+
+  export let events: Event;
 </script>
 
 <div class="my-4">
